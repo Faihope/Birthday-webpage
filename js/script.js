1f9fe5359ef6cd8666e23eb05eebf50f7ac3c462
Fai <chepngetichfaith46@gmail.com>
2021-04-11 20:09:48 +0300

    Add a function to validate date

--- js/script.js
+++ js/script.js
@@ -1,80 +1,19 @@
-var female = function () {
-    [
-        {
-            name1: "Sunday",
-            akan: "Kwasi"
-        },
-        {
-            name2: "Monday",
-            akan: "Kwadwo"
-        },
-        {
-            name3: "Tuesday",
-            akan: "kwabena"
-        },
-        {
-            name4: "Wednesday",
-            akan: "Kwaku"
-        },
-        {
-            name5: "Thursday",
-            akan: "Yaw"
-        },
-        {
-            name6: "Friday",
-            akan: "Kofi"
-        },
-        {
-            name7: "Sartuday",
-            akan: "Kwame"
+
+function OnSubmit(){
+    console.log("submitted");
+}
+
+function validDate() {
+    var date = document.getElementById("Date").value;
+    if (date) {
+        if (date >= 1 && date <= 31) {
+            return date;
+        } else {
+            alert("date should be 1-31");
         }
-    ]
-}
+    } else {
+        alert("please enter date")
+    }
 
-
-function male(days, akan) {
-
-    var sunday = {
-        day: "Sunday",
-        akan: "Kwasi"
-    };
-    var monday = {
-        day: "Monday",
-        akan: "Kwadwo"
-    };
-    var tuesday = {
-        day: "Tuesday",
-        akan: "Kwabena"
-    };
-    var wednesday = {
-        day: "Wednesday",
-        akan: "Kwaku"
-    };
-    var thursday = {
-        day: "Thursday",
-        akan: "Yaw"
-    };
-    var friday = {
-        day: "Friday",
-        akan: "Kofi"
-    };
-    var sartuday = {
-        day: "Sartuday",
-        akan: "Kwame"
-    };
-    var days = [sunday, monday, tuesday, wednesday, thursday, friday, sartuday];
-    days=document.getElementById("akan").value;
 
 }
-``
-
-document.getElementById("birthday").value;
-document.getElementById("mygender").value;
-
-document.getElementById("akan").innerHTML +="your akan name is" + sunday.akan;
-
- if ((sunday.day ) && (male()) ){
-    alert(days) ;
-
- }
-
