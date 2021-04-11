0dabc95c3cdef4eaaa95044b2dc290b3c931c808
Fai <chepngetichfaith46@gmail.com>
2021-04-11 19:56:09 +0300

    Delete practice files and the content of script.js

--- js/script.js
+++ js/script.js
@@ -1,63 +1,80 @@
-var male= function(){[
-    {
-        name:"Sunday",
-        akan:"Kwasi"
-    },
-    {
-        name:"Monday",
-        akan:"Kwadwo"
-    },
-    {
-        name:"Tuesday",
-        akan:"kwabena"
-    },
-    {
-        name:"Wednesday",
-        akan:"Kwaku"
-    },
-    {
-        name:"Thursday",
-        akan:"Yaw"
-    },
-    {
-        name:"Friday",
-        akan:"Kofi"
-    },
-    {
-        name:"Sartuday",
-        akan:"Kwame"
-    }
-]}
+var female = function () {
+    [
+        {
+            name1: "Sunday",
+            akan: "Kwasi"
+        },
+        {
+            name2: "Monday",
+            akan: "Kwadwo"
+        },
+        {
+            name3: "Tuesday",
+            akan: "kwabena"
+        },
+        {
+            name4: "Wednesday",
+            akan: "Kwaku"
+        },
+        {
+            name5: "Thursday",
+            akan: "Yaw"
+        },
+        {
+            name6: "Friday",
+            akan: "Kofi"
+        },
+        {
+            name7: "Sartuday",
+            akan: "Kwame"
+        }
+    ]
+}
 
 
-var female= function(){[
-    {
-        name:"Sunday",
-        akan:"Akosua"
-    },
-    {
-        name:"Monday",
-        akan:"Adwoa"
-    },
-    {
-        name:"Tuesday",
-        akan:"Abenaa"
-    },
-    {
-        name:"Wednesday",
-        akan:"Akua"
-    },
-    {
-        name:"Thursday",
-        akan:"Yaa"
-    },
-    {
-        name:"Friday",
-        akan:"Afua"
-    },
-    {
-        name:"Sartuday",
-        akan:"Ama"
-    }
-]}
+function male(days, akan) {
+
+    var sunday = {
+        day: "Sunday",
+        akan: "Kwasi"
+    };
+    var monday = {
+        day: "Monday",
+        akan: "Kwadwo"
+    };
+    var tuesday = {
+        day: "Tuesday",
+        akan: "Kwabena"
+    };
+    var wednesday = {
+        day: "Wednesday",
+        akan: "Kwaku"
+    };
+    var thursday = {
+        day: "Thursday",
+        akan: "Yaw"
+    };
+    var friday = {
+        day: "Friday",
+        akan: "Kofi"
+    };
+    var sartuday = {
+        day: "Sartuday",
+        akan: "Kwame"
+    };
+    var days = [sunday, monday, tuesday, wednesday, thursday, friday, sartuday];
+    days=document.getElementById("akan").value;
+
+}
+``
+
+document.getElementById("birthday").value;
+document.getElementById("mygender").value;
+
+document.getElementById("akan").innerHTML +="your akan name is" + sunday.akan;
+
+ if ((sunday.day ) && (male()) ){
+    alert(days) ;
+
+ }
 
